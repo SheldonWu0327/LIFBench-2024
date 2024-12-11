7deeeb8a272e2ec54f18838c8522543bc7e3bf4b
SheldonWu0327 <wxd0327@hotmail.com>
2024-12-12 00:12:33 +0800

    fix to param

--- scripts/generate_prompts.sh
+++ scripts/generate_prompts.sh
@@ -1,7 +1,7 @@
 # construct the contexts $X$ in different length interval. Results will be in "{data_dir}/meta/".
 python data/ConstructContext.py \
   --base_dir "." \  # Base dir of LIFBench.
-  --context_lengths [3,14,28] # The length interval of the context $X$ to be constructed.
+  --context_lengths [3,6,14,28,60,120] # The length interval of the context $X$ to be constructed.
 
 # Concatenate all components to form the prompt. Results will be in "{data_dir}/prompts/".
 python data/PromptGenerate.py --base_dir '.'
